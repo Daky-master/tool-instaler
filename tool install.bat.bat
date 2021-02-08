2ba3c8f899871ee848287f365366a92d07d3941a
Daky-master <62757531+Daky-master@users.noreply.github.com>
2021-02-08 19:20:09 +0100

    Add files via upload

---
+++ tool install.bat.bat
@@ -0,0 +1,29 @@
+@echo off
+git clone https://github.com/sherlock-project/sherlock.git
+git clone https://github.com/arismelachroinos/lscript.git
+git clone https://github.com/epsylon/ufonet.git
+git clone https://github.com/vanhauser-thc/thc-hydra.git
+git clone https://github.com/AzizKpln/Bruter19.git
+git clone https://github.com/Bitwise-01/Instagram-.git
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce.git
+git clone https://github.com/0xfff0800/Brute-Forc-Twitter-.git
+git clone https://github.com/NewEraCracker/LOIC.git
+git clone https://github.com/DynamicDesignz/HOIC.git
+git clone https://github.com/Mr4FX/HULK.git
+git clone https://github.com/649/Memcrashed-DDoS-Exploit.git
+git clone https://github.com/UBoat-Botnet/UBoat.git
+git clone https://github.com/malwaredllc/byob.git
+git clone https://github.com/The404Hacking/ZIP-Password-BruteForcer.git
+goto a
+:a
+cls
+color a
+sucsefuly instaled
+pause
+exit
+
+
+
+
+
+
